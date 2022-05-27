416a7c0c9cacb60f14d45db4e64e311baae3816c
doanjulia <dathaodoanngoc310@gmail.com>
2022-05-27 14:51:16 +0700

    changes

--- pos/src/App.vue
+++ pos/src/App.vue
@@ -34,25 +34,11 @@ export default {
     myCards,
     myFooter,
     fabButton,
-  },
-  data() {
-    return {
-      header: 'initial header'
-    }
   }
-
-  
 }
 
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
